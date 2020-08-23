bb88ed7fa2efc4ca468ec4996de2bd93a2838382
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-08-23 22:21:15 +0100

    Add project files.

---
+++ AppIndexedDbFolder/MoviesDb.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.IndexedDB.Framework;
+using IndexedDBApp.Models;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndexedDBApp.AppIndexedDbFolder
+{
+    public class MoviesDb : IndexedDb
+    {
+        public MoviesDb(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) { }
+
+    // These are like tables. Declare as many of them as you want.    
+        public IndexedSet<MovieModel> Movies { get; set; }       
+    }
+
+
+}
---
+++ Models/MovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndexedDBApp.Models
+{
+    public class MovieModel
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
